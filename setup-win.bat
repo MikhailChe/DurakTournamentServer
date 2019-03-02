0acc0fd91325b4506f5df1e9df8544aeaa710e57
Mikhail Chernoskutov <mikhailche@yandex-team.ru>
2019-03-02 12:22:28 +0500

    Launch and update pip via python.exe

--- setup-win.bat
+++ setup-win.bat
@@ -1,5 +1,6 @@
 py -3 -m venv --clear env
-env\Scripts\pip.exe install -r requirements.txt
+env\Scripts\python.exe -m pip install -r requirements.txt
+env\Scripts\python.exe -m pip install -r requirements.txt
 pushd gameserver
 ..\env\Scripts\python.exe manage.py migrate
 ..\env\Scripts\python.exe manage.py createsuperuser
