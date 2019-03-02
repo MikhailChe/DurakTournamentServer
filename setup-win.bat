e2795b46f0ec41a028edbf1038d0f47930538812
Mikhail Chernoskutov <mikhailche@yandex-team.ru>
2019-03-02 10:19:41 +0500

    Exit on error. Better python version choice

--- setup-win.bat
+++ setup-win.bat
@@ -1,7 +1,16 @@
-virtualenv -p python3.7 --clear env
+setlocal enabledelayedexpansion
+if %errorlevel% neq 0 exit /b %errorlevel%
+python3 -m venv --clear env
+if %errorlevel% neq 0 exit /b %errorlevel%
 ./env/Scripts/activate.bat
+if %errorlevel% neq 0 exit /b %errorlevel%
 pip install --upgrade pip
+if %errorlevel% neq 0 exit /b %errorlevel%
 pip install -r requirements.txt
+if %errorlevel% neq 0 exit /b %errorlevel%
 pushd gameserver
+if %errorlevel% neq 0 exit /b %errorlevel%
 python manage.py migrate
+if %errorlevel% neq 0 exit /b %errorlevel%
 popd
+if %errorlevel% neq 0 exit /b %errorlevel%
